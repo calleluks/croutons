cacab978229b3571a84f78b1c66bdcd9507dfe9d
Calle Luks <calle@luks.in>
2025-08-26 08:35:59 +0200

    Add support for Rails 8
    The way we were getting the virtual_path of the template no longer works
    in Rails 8. When defining the breadcrumbs helper within a helper
    do-block (effectively defining it on the controller's helper module) we
    get access to @virtual_path defined within the calling view.

--- lib/croutons/controller.rb
+++ lib/croutons/controller.rb
@@ -1,36 +1,27 @@
 module Croutons
   module Controller
     def self.included(controller)
-      controller.helper_method(:breadcrumbs)
-    end
+      controller.helper do
+        def breadcrumbs(objects = {})
+          begin
+            breadcrumb_trail = ::BreadcrumbTrail
+          rescue NameError
+            raise NotImplementedError,
+              'Define a `BreadcrumbTrail` class that inherits from '\
+              '`Breadcrumbs::BreadcrumbTrail`, or override the '\
+              '`breadcrumb_trail` method in your controller so that it '\
+              'returns an object that responds to `#breadcrumbs`.'
+          end
 
-    def render_to_body(options)
-      @_template = options[:template]
-      @_prefixes = options[:prefixes]
-      super
-    end
-
-    private
-
-    def breadcrumbs(objects = {})
-      template = lookup_context.find_template(@_template, @_prefixes)
-      template_identifier = template.virtual_path.gsub('/', '_')
-      objects.reverse_merge!(view_assigns)
-      breadcrumbs = breadcrumb_trail.breadcrumbs(template_identifier, objects)
-      render_to_string(
-        partial: 'breadcrumbs/breadcrumbs',
-        locals: { breadcrumbs: breadcrumbs },
-      )
-    end
-
-    def breadcrumb_trail
-      ::BreadcrumbTrail
-    rescue NameError
-      raise NotImplementedError,
-        'Define a `BreadcrumbTrail` class that inherits from '\
-        '`Breadcrumbs::BreadcrumbTrail`, or override the '\
-        '`breadcrumb_trail` method in your controller so that it '\
-        'returns an object that responds to `#breadcrumbs`.'
+          template_identifier = @virtual_path.gsub('/', '_')
+          objects.reverse_merge!(controller.view_assigns)
+          breadcrumbs = breadcrumb_trail.breadcrumbs(template_identifier, objects)
+          render(
+            partial: 'breadcrumbs/breadcrumbs',
+            locals: { breadcrumbs: breadcrumbs },
+          )
+        end
+      end
     end
   end
 end
